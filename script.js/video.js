4f3d571433b19898ad472ceb6a70252159ba29fa
RefatRingky <rifatsourav43@gmail.com>
2025-04-29 01:19:32 +0600

    format the time

--- script.js/video.js
+++ script.js/video.js
@@ -26,11 +26,17 @@ const displayVideos =(videos) =>{
        card.innerHTML=
        `
         
-      <figure class="h-[200px]">
+      <figure class="h-[200px] relative">
     <img
       src=${video.thumbnail}
       class="h-full w-full object-cover"
       alt="Shoes" />
+      ${
+        video.others.posted_date?.length == 0 ? "" :`
+        <span class="absolute  right-2  bottom-2 rounded text-white bg-black p-1">${video.others.posted_date}</span>
+        `
+      }
+       
   </figure>
       <div
         class="px-0 py-2 flex gap-2">
@@ -43,7 +49,9 @@ const displayVideos =(videos) =>{
         </h2>
         <div class="flex items-center gap-2">
         <p class="text-gray-400">${video.authors[0].profile_name}</p>
-        <img class="h-5 w-5" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png" />
+        ${video.authors[0].verified == true ? `<img class="h-5 w-5" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png" />
+          `: ""}
+        
         </div>
       </div>
       </div>
