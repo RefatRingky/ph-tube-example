9af904e651c8e6077ebebda9982ea998df1f929c
RefatRingky <rifatsourav43@gmail.com>
2025-04-30 01:37:18 +0600

    show conditional no content text

--- script.js/video.js
+++ script.js/video.js
@@ -28,6 +28,21 @@ const loadCategoryVideos = (id) => {
 const displayVideos = (videos) => {
   const videoContainer = document.getElementById("videos")
   videoContainer.innerHTML="";
+
+  if(videos.length == 0){
+    videoContainer.classList.remove("grid")
+    videoContainer.innerHTML=`
+    <div class="min-h-[300px] flex flex-col gap-5 justify-center items-center">
+       <img src="assets/Icon.png"  />
+    </div>
+    <h2 class="text-center text-xl font-bold">No Content Here In This Category</h2>
+    
+    `;
+    return; 
+  }
+  else{
+    videoContainer.classList.add("grid")
+  }
   videos.forEach((video) => {
     console.log(video)
     const card = document.createElement("div")
